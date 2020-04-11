b4d247ab12238d65efaf37e009c691f58d64342e
Jono Hill <jonathan@hillnz.com>
2020-04-11 14:39:59 +1200

    Fix wifi

--- config/wifi.sh
+++ config/wifi.sh
@@ -7,4 +7,14 @@ set -e
 mkdir -p /etc/wpa_supplicant || true
 printf "${WIFI_PASSWORD}" | wpa_passphrase "${WIFI_SSID}" >/etc/wpa_supplicant/wpa_supplicant.conf
 
+cat >>/etc/network/interfaces <<EOF
+
+allow-hotplug wlan0
+iface wlan0 inet manual
+    wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+
+EOF
+
+systemctl enable wpa_supplicant.service
+
 echo End wifi
